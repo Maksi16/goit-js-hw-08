e900285aa11ea3087130264ee7cc7196400dfb9c
Maksi16 <maksim.moroz.kr@gmail.com>
2022-08-08 00:59:21 +0300

    task 01

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/common.css';
+import './css/01-gallery.css';
+
+import './js/01-gallery';
--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgEl = galleryItems
+  .map(galleryItem => {
+    const itemEl = `
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+    />
+  </a>`;
+    return itemEl;
+  })
+  .join(' ');
+
+const galleryBox = document.querySelector('.gallery');
+
+galleryBox.innerHTML = imgEl;
+galleryBox.addEventListener('click', onModalOpen);
+
+function onModalOpen(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
